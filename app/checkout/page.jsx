21a49980252b92438c0a5ddb92cc971c711a51cd
Maro-Al-Kades <97260148+Maro-Al-Kades@users.noreply.github.com>
2024-02-07 17:24:18 +0200

    Update page.jsx

--- app/checkout/page.jsx
+++ app/checkout/page.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import React, { Suspense } from "react";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import CheckoutForm from "./_components/CheckoutForm";
@@ -7,18 +8,29 @@ import { useSearchParams } from "next/navigation";
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHER_KEY);
 
-function Checkout() {
+function CheckoutContent() {
   const searchParams = useSearchParams();
+
+  const amount = Number(searchParams.get("amount")) || 0; // توفير قيمة افتراضية في حال لم يتم تحديد المبلغ
 
   const options = {
     mode: "payment",
     currency: "usd",
-    amount: Number(searchParams.get("amount")) * 100,
+    amount: amount * 100,
   };
+
   return (
     <Elements stripe={stripePromise} options={options}>
-      <CheckoutForm amount={Number(searchParams.get("amount"))} />
+      <CheckoutForm amount={amount} />
     </Elements>
+  );
+}
+
+function Checkout() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <CheckoutContent />
+    </Suspense>
   );
 }
 
